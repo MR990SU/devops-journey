81055415d20fd6e667b574f53a18392860673e9b
MRSU990 <mr.su990@gmail.com>
2026-03-17 20:32:21 +0530

    log analyzer updated

--- bash/log-analyzer.sh
+++ bash/log-analyzer.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-report="report.txt"
-logs="../logs/dumy.log"
+srcdir=$(dirname "$(realpath "$0")")
+report="$srcdir/report.txt"
+logs="$srcdir/../logs/dumy.log"
 echo "Log-analyzer Started at $(date)" >$report
+if [ ! -f $logs ]
+then
+	echo "logs not found" >$report
+	exit 1
+fi
 errorcount=$(grep -c -i "error" $logs)
 infocount=$(grep -c -i "info" $logs)
 warningcount=$(grep -c -i "warning" $logs)
@@ -10,5 +16,6 @@ echo "Total error count: $errorcount" >>$report
 echo "Total warning count: $warningcount" >>$report
 echo "Total info count: $infocount" >>$report
 echo "Script Completed at $(date)" >>$report
+echo "Report generated $report"
 echo "Script Completed at $(date)"
 
